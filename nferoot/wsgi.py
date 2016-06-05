da143607bd7ee0e70a35f879b9d9eca5e097b201
Guilherme Bazilio <guilhermebazilio@gmail.com>
2016-06-04 21:04:41 -0300

    runtime 3.5

--- nferoot/wsgi.py
+++ nferoot/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nferoot.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nferoot.settings.heroku")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
